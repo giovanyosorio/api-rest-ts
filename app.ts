f08eacb59ffca64a157ff0e4f1c1946bcb9e9344
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-04-01 17:32:43 +0000

    estructura de carpetas y configuracion de app.ts

---
+++ app.ts
@@ -0,0 +1,12 @@
+import "dotenv/config"
+import express from "express"
+import cors from "cors"
+const PORT=process.env.PORT || 3000
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(PORT,()=>{
+    console.log(`listening on PORT ${PORT}`)
+})
