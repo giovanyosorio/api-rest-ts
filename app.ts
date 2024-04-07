44313afd1cad3f4411dcec9eeb6071939f95e4e6
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-04-06 21:23:06 -0500

    mongodb

--- app.ts
+++ app.ts
@@ -2,12 +2,14 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import {router} from "./src/routes"
-
+import db from "./src/config/mongo"
 const PORT=process.env.PORT || 3000
 const app=express()
 app.use(router)
 app.use(express.json())
 app.use(cors())
+db().then(()=>console.log("succesfull"))
+
 
 app.listen(PORT,()=>{
     console.log(`listening on PORT ${PORT}`)
