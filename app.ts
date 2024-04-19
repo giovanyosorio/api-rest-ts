1cacffff2ed27391626aa898dd420c10d1a2c021
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-04-19 16:30:04 -0500

    Connecccting to database

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ const app=express()
 app.use(router)
 app.use(express.json())
 app.use(cors())
-db().then(()=>console.log("succesfull"))
+db().then(()=>console.log("Connecting to the database"))
 
 
 app.listen(PORT,()=>{
